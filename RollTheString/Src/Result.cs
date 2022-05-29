7677dd7507f858b4261ce9d90b9645d9313642a4
andrey kurochkin <andrey.kurochkin@hotmail.com>
2022-05-29 12:17:02 +0300

    initial commit

---
+++ RollTheString/Src/Result.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RollTheString.Src;
+public class Result
+{
+    public static string RollTheString(string s, List<int> rolls)
+    {
+        throw new NotImplementedException();
+    }
+}
